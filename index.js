780021992392dfdab9ee58931ee092ab3468a1d4
Sean Elwood <seanelwood@gmail.com>
2017-11-17 16:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function BoardMember (name, homeState, training){
   this.approve = function(){
     return 'You can do that!';
   }
-  this.releasePressStatement = function(){
-    return 'You will see great things from Scuber.'
-  }
   this.doCharity = function(){
     return 'I like to help people.'
+  }
+  this.releasePressStatement = function(){
+    return 'You will see great things from Scuber.'
   }
   this.sayHi = function(){
     return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
