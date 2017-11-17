406655b23a722d7e4a9d17ee375b129275c7949f
Sean Elwood <seanelwood@gmail.com>
2017-11-17 16:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function BoardMember (name, homeState, training){
   this.veto = function(){
     return 'No, I must disagree';
   }
+  this.veto = function(){
+    return 'You can do that!';
+  }
 }
 
 
