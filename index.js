b29840869a5cff782532ecbdf73bd3a7ae3306a6
Sean Elwood <seanelwood@gmail.com>
2017-11-17 16:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-function BoardMember (name, homeState, training) {
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
-
+function Boardmember(name, homeState, training){
   this.veto = function(){
-    return `No, I must disagree`;
-  }
-  this.approve = function(){
-    return `You can do that!`;
-  }
-  this.doCharity = function(){
-    return `I like to help people.`;
-  }
-  this.releasePressStatement = function(){
-    return `You will see great things from Scuber.`;
-  }
-  this.sayHi = function(){
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
+    return 'No, I must disagree';
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+// function BoardMember (name, homeState, training) {
+//   this.name = name;
+//   this.homeState = homeState;
+//   this.training = training;
+// 
+//   this.veto = function(){
+//     return `No, I must disagree`;
+//   }
+//   this.approve = function(){
+//     return `You can do that!`;
+//   }
+//   this.doCharity = function(){
+//     return `I like to help people.`;
+//   }
+//   this.releasePressStatement = function(){
+//     return `You will see great things from Scuber.`;
+//   }
+//   this.sayHi = function(){
+//     return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
+//   }
+// }
